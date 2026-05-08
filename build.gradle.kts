d57d2f11e127047c95923a0545dbdc289ff3c327
outusuke <35202858+outusuke@users.noreply.github.com>
2026-05-08 14:49:35 -0300

    Update Kotlin Gradle plugin and Java compatibility
    Signed-off-by: outusuke <35202858+outusuke@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,10 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
-
 allprojects {
     repositories {
         google()
@@ -19,37 +18,29 @@ allprojects {
         maven { url = uri("https://jitpack.io") }
     }
 }
-
 fun Project.cloudstream(configuration: com.lagradost.cloudstream3.gradle.CloudstreamExtension.() -> Unit) = 
     extensions.getByName<com.lagradost.cloudstream3.gradle.CloudstreamExtension>("cloudstream").configuration()
-
 fun Project.android(configuration: com.android.build.gradle.BaseExtension.() -> Unit) = 
     extensions.getByName<com.android.build.gradle.BaseExtension>("android").configuration()
-
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
-
     cloudstream {
-        setRepo("https://github.com/lawlietbr/lietrepo")
+        setRepo("https://github.com/YOUR_USERNAME/lietrepo")
         authors = listOf("lawlietbr")
     }
-
     android {
         namespace = "com.lietrepo"
-
         defaultConfig {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
         }
-
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_6
-            targetCompatibility = JavaVersion.VERSION_1_6
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
-
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             compilerOptions {
                 jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
@@ -61,13 +52,10 @@ subprojects {
             }
         }
     }
-
     dependencies {
         val implementation by configurations
         val cloudstream by configurations
         cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        // Other dependencies
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.13")
         implementation("org.jsoup:jsoup:1.19.1")
@@ -79,10 +67,9 @@ subprojects {
         implementation("com.google.code.gson:gson:2.11.0")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
         implementation("app.cash.quickjs:quickjs-android:0.9.2")
-       implementation("com.github.vidstige:jadb:v1.2.1")
+        implementation("com.github.vidstige:jadb:v1.2.1")
     }
 }
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
